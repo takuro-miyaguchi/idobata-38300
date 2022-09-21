0d7048b675bf7f849cf85e029c2bdd76351e2a39
trackmaintenance <trackmainte@gmail.com>
2022-09-21 11:25:33 +0900

    ユーザー登録・ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :prefecture_id, :municipality, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :prefecture_id, :municipality, :address])
   end
 
 end
